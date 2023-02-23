446e2079404174a1af99fd7199c1a3b329fd6fba
Cristobal Mejia <cristobal.mejia@gmail.com>
2023-02-23 01:11:56 -0600

    express

--- src/index.js
+++ src/index.js
@@ -1,18 +1,34 @@
+import express from "express";
 import request from 'request';
 
-const options = {
-  method: 'GET',
-  url: 'https://api.coingecko.com/api/v3/simple/price',
-  qs: {ids: 'ripple', vs_currencies: 'usd'},
-  headers: {
-    useQueryString: true
-  }
-};
 
-request(options, function (error, response, body) {
-	if (error) throw new Error(error);
 
-	console.log(body);
+const app = express();
+const port = 3000;
 
-    return 1;
+app.get("/", (req, res) => {
+  
 });
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+  call();
+});
+
+function call() {
+    const options = {
+    method: 'GET',
+    url: 'https://api.coingecko.com/api/v3/simple/price',
+    qs: {ids: 'ripple', vs_currencies: 'usd'},
+    headers: {
+        useQueryString: true
+    }
+    };
+
+    request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+
+        console.log(body);
+
+    });
+}
