f5671a16d1a28cfe3d6fd8665579407296b0e2aa
Cristobal Mejia <cristobal.mejia@gmail.com>
2023-02-23 00:32:59 -0600

    start

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const request = require('request');
+
+const options = {
+  method: 'GET',
+  url: 'https://api.coingecko.com/api/v3/simple/price',
+  qs: {ids: 'ripple', vs_currencies: 'usd'},
+  headers: {
+    useQueryString: true
+  }
+};
+
+request(options, function (error, response, body) {
+	if (error) throw new Error(error);
+
+	console.log(body);
+});
